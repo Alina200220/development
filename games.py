d8ceed2de4ba8d67a7fbd312d7915d1fe727cdcd
Alina <fakherdinova.alya@mail.ru>
2024-09-27 19:40:44 +0300

    added linter

--- games.py
+++ games.py
@@ -1,30 +1,34 @@
 from random import randint
 from typing import Tuple
 
-def nok_game() -> Tuple[str, int]: 
+
+def nok_game() -> Tuple[str, int]:
     """Логика игры по нахождению НОК"""
+
     def get_nok(a, b):
-      c = a * b
-      # поиск нод
-      while a != 0 and b != 0:
-         b, a = max(a, b) % min(a, b), min(a, b)
-      return c // (a + b)
+        c = a * b
+        # поиск нод
+        while a != 0 and b != 0:
+            b, a = max(a, b) % min(a, b), min(a, b)
+        return c // (a + b)
+
     lst = [str(randint(0, 100)) for _ in range(3)]
     answer = get_nok(int(lst[0]), get_nok(int(lst[1]), int(lst[2])))
     question_string = " ".join(lst)
     return question_string, answer
 
+
 def geometry_progression() -> Tuple[str, int]:
-   """Логика игры геометрическая прогрессия"""
-   q = randint(1, 10) #знаменатель прогрессии
-   b_first = randint(1, 10) #первый элемент прогрессии
-   progression_elements = [b_first]
-   for i in range(1, 10):
-      progression_elements.append(progression_elements[i-1]*q) #геометрическая прогрессия из 10 элементов
-   index_of_element_to_replace = randint(0, 9)
-   correct_answer = progression_elements[index_of_element_to_replace]
-   progression_elements[index_of_element_to_replace] = ".."
-   question_string = " ".join(list(map(str, progression_elements)))
-   return question_string, correct_answer
-
-
+    """Логика игры геометрическая прогрессия"""
+    q = randint(1, 10)  # знаменатель прогрессии
+    b_first = randint(1, 10)  # первый элемент прогрессии
+    progression_elements = [b_first]
+    for i in range(1, 10):
+        progression_elements.append(
+            progression_elements[i - 1] * q
+        )  # геометрическая прогрессия из 10 элементов
+    index_of_element_to_replace = randint(0, 9)
+    correct_answer = progression_elements[index_of_element_to_replace]
+    progression_elements[index_of_element_to_replace] = ".."
+    question_string = " ".join(list(map(str, progression_elements)))
+    return question_string, correct_answer
--- source.py
+++ source.py
@@ -4,7 +4,6 @@ from games import geometry_progression, nok_game
 
 
 class GameInteface:
-    
     @staticmethod
     def greeting():
         print("Welcome to the Brain Games! ")
@@ -12,32 +11,36 @@ class GameInteface:
         name = input()
         print(f"Hello {name}!")
         return name
-    
+
     @staticmethod
-    def check_answer(correct_answer, user_answer, user_name:str) -> bool:
+    def check_answer(correct_answer, user_answer, user_name: str) -> bool:
         """Функция сравнивает правильный ответ и пользователя, выводит соответствующие строки"""
         print(f"Your answer: {user_answer}")
         if int(user_answer) == int(correct_answer):
             print("Correct!")
             return True
         else:
-            print(f"{user_answer} is wrong answer ;(. Correct answer was {correct_answer}.")
+            print(
+                f"{user_answer} is wrong answer ;(. Correct answer was {correct_answer}."
+            )
             print(f"Let's try again, {user_name}")
             return False
-    
+
     @staticmethod
-    def one_round(user_name:str, game_fuction:Callable) -> bool:
+    def one_round(user_name: str, game_fuction: Callable) -> bool:
         for j in range(3):
             question_string, correct_answer = game_fuction()
             print(f"Question: {question_string}")
             user_answer = input()
             answer = GameInteface.check_answer(correct_answer, user_answer, user_name)
-            if answer is False: #если пользователь дал неверный ответ, то три раунда заново
+            if (
+                answer is False
+            ):  # если пользователь дал неверный ответ, то три раунда заново
                 return False
         return True
 
     @staticmethod
-    def game_interface(game_description:str, game_fuction:Callable):
+    def game_interface(game_description: str, game_fuction: Callable):
         """Общий интерфейс для всех игр, сама функции игры передается аргументом и вызывается в 39 строчке. Всего три раунда по три вопроса в каждом"""
         user_name = GameInteface.greeting()
         print(game_description)
@@ -47,8 +50,11 @@ class GameInteface:
                 print("Congratulations!")
                 return
         print("You lost 3 rounds")
-                  
 
-GameInteface.game_interface("Find the smallest common multiple of given numbers.", nok_game)
-GameInteface.game_interface("What number is missing in the progression?", geometry_progression)
-    
+
+GameInteface.game_interface(
+    "Find the smallest common multiple of given numbers.", nok_game
+)
+GameInteface.game_interface(
+    "What number is missing in the progression?", geometry_progression
+)
